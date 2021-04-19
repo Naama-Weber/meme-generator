cebed36edb1a2b7d72798681a512bed20348bb95
Naama Weber <naamaw99@gmail.com>
2021-04-19 18:47:21 +0300

    first phase

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,57 @@
 'use strict';
+
+var gCanvas;
+var gCtx;
+var gCurrImg;
+
+function onInit() {
+    gCanvas = document.querySelector('#canvas');
+    gCtx = gCanvas.getContext('2d');
+    renderGallery();
+}
+
+function renderGallery() {
+    var imgs = getImgsForDisplay();
+    var strHTMLs = imgs.map((img) => {
+        return `<div><img class="meme-img img-${img.id}" src="images/memes/${img.url}" onclick="onDrawImg('${img.id}')"></div>`
+    });
+    var elGalleryContainer = document.querySelector('.gallery-container');
+    elGalleryContainer.innerHTML = strHTMLs.join('');
+}
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    gCanvas.width = elContainer.offsetWidth;
+    gCanvas.height = elContainer.offsetHeight;
+}
+
+function onDrawImg(imgId) {
+    // gCurrImg = getImgById(imgId);
+    // console.log(gCurrImg)
+    updateSelectedImg(+imgId);
+    resizeCanvas();
+    var elImg = document.querySelector(`.img-${imgId}`);
+    gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
+}
+
+function onEditMemeText(text) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'black';
+    gCtx.fillStyle = 'white';
+    gCtx.font = '50px impact';
+    gCtx.textAlign = 'left';
+    updateLines(text, gCtx.font, gCtx.textAlign, gCtx.fillStyle);
+    let lines = getLines();
+    let txt = lines.txt;
+    onDrawText(txt, 100, 100);
+}
+
+function onDrawText(text, x, y) {
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+function downloadMeme(elLink) {
+    var imgContent = canvas.toDataURL();
+    elLink.href = imgContent;
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,74 @@
 'use strict';
+
+var gKeywords = { 'happy': 12, 'funny': 1 }
+var gImgs = [
+    { id: 1, url: '1.jpg', keywords: ['politics'] },
+    { id: 2, url: '2.jpg', keywords: ['cute', 'animals'] },
+    { id: 3, url: '3.jpg', keywords: ['cute', 'animals'] },
+    { id: 4, url: '4.jpg', keywords: ['cute', 'animals'] },
+    { id: 5, url: '5.jpg', keywords: ['cute', 'animals'] },
+    { id: 6, url: '6.jpg', keywords: ['cute', 'animals'] },
+    { id: 7, url: '7.jpg', keywords: ['cute', 'animals'] },
+    { id: 8, url: '8.jpg', keywords: ['cute', 'animals'] },
+    { id: 9, url: '9.jpg', keywords: ['cute', 'animals'] },
+    { id: 10, url: '10.jpg', keywords: ['cute', 'animals'] },
+    { id: 11, url: '11.jpg', keywords: ['cute', 'animals'] },
+    { id: 12, url: '12.jpg', keywords: ['cute', 'animals'] },
+    { id: 13, url: '13.jpg', keywords: ['cute', 'animals'] },
+    { id: 14, url: '14.jpg', keywords: ['cute', 'animals'] },
+    { id: 15, url: '15.jpg', keywords: ['cute', 'animals'] },
+    { id: 16, url: '16.jpg', keywords: ['cute', 'animals'] },
+    { id: 17, url: '17.jpg', keywords: ['cute', 'animals'] },
+    { id: 18, url: '18.jpg', keywords: ['cute', 'animals'] }
+];
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'default',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
+
+var gCurrLine = 1;
+
+function getMeme() {
+    return gMeme;
+}
+
+// function getImgById(imgId) {
+//     return gImgs.find(function (img) {
+//         return img.id === imgId
+//     });
+// }
+    
+function getImgsForDisplay() {
+    return gImgs;
+}
+
+function updateSelectedImg(imgId) {
+    gMeme.selectedImgId = imgId;
+}
+
+function updateSelectedLineIdx(line) {
+    gMeme.selectedLineIdx = line;
+    gCurrLine = line;
+}
+
+function updateLines(txt, size, align, color) {
+    gMeme.lines[0].txt = txt;
+    gMeme.lines[0].size = size;
+    gMeme.lines[0].align = align;
+    gMeme.lines[0].color = color;
+}
+
+function getLines() {
+    return gMeme.lines[0];
+}
+
+
+
